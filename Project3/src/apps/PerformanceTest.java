54acddbb7c51349e12391a2862adafbc7c33eb3b
Alex <therealdeal1231@gmail.com>
2020-03-31 14:42:24 -0400

    Update PerformanceTest.java

--- Project3/src/apps/PerformanceTest.java
+++ Project3/src/apps/PerformanceTest.java
@@ -7,6 +7,7 @@ public class PerformanceTest {
 	public static void main(String[] args) {
 		
 		int size = 30000;
+		int avg = 0;
 		LLBasedList<Integer> list = new LLBasedList<>();
 		for (int i = 0; i < size; i++) {
             		list.add(i);
@@ -20,23 +21,26 @@ public class PerformanceTest {
 		for (int i = 0; i < 5; i++) {
 			int num = (int) (Math.random() * size);
 			StartTime = System.nanoTime();
-			list.find1(num);
+			list.contains(num);
 			EndTime = System.nanoTime();
-			
+			avg += EndTime - StartTime;
 			System.out.println("Time in microseconds to search for random number " + num + " using a linear search: " + (EndTime - StartTime) / 1000 + " microseconds");
 		}
-		
+		System.out.println("Average time for linear search: " + (avg/5)/1000 + " microseconds");
 		System.out.println();
 	    System.out.println("Search for " + size);
 	    
+	    list.setFind(2);
+	    avg = 0;
 	    for (int i = 0; i < 5; i++) {
 	    	int num = (int) (Math.random() * size);
 	    	StartTime = System.nanoTime();
-	    	list.find2(num);
+	    	list.contains(num);
 	    	EndTime = System.nanoTime();
-	    	
+	    	avg += EndTime - StartTime;
 	    	System.out.println("Time in microseconds to search for random number " + num + " using a binary search: " + (EndTime - StartTime) / 1000 + " microseconds");
 	    }
+	    System.out.println("Average time for binary search: " + (avg/5)/1000 + " microseconds");
 	    
 	}
 
